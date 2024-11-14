cef1ad70cc360e1a2d095d89a29a2343f4fd500c
Woo <gwoprk98@gmail.com>
2024-11-14 20:58:28 +0900

    feat: UserDao 기능 수정

---
+++ src/test/java/springbook/user/jdbctemplate/UserDaoTest.java
@@ -0,0 +1,41 @@
+package springbook.user.jdbctemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import springbook.user.domain.User;
+import springbook.user.jdbc.DaoConfig;
+import springbook.user.jdbc.UserDao;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = DaoConfig.class)
+class UserDaoTest {
+
+    @Autowired
+    private UserDao userDao;
+
+    @BeforeEach
+    void delete() {
+        userDao.deleteAll();
+    }
+
+    @DisplayName(value = "사용자 조회하기")
+    @Test
+    void findById() {
+        // given
+        User user = new User("id", "name", "1234");
+        userDao.add(user);
+
+        // when
+        User actual = userDao.findById(user.getId());
+
+        // then
+        assertThat(actual).isEqualTo(user);
+    }
+}
