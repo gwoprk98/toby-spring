4f8d616ae043fe122577d359523183d574b530ba
Woo <gwoprk98@gmail.com>
2024-11-29 13:51:36 +0900

    feat: InvocationHandler를 이용해 다이나믹 프록시 생성

--- src/test/java/springbook/proxy/WordTest.java
+++ src/test/java/springbook/proxy/WordTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Proxy;
+
 public class WordTest {
 
     @DisplayName(value = "ProxyWord를 이용한 word 테스트")
@@ -17,5 +19,20 @@ public class WordTest {
         assertThat(word.sayHello()).isEqualTo("HELLO GWOOPRK!");
         assertThat(word.sayThanks()).isEqualTo("THANK YOU GWOOPRK!");
 
+    }
+
+    @DisplayName(value = "InvokeHandler를 이용해 다이나믹 프록시 생성")
+    @Test
+    void dynamicProxy() {
+        // given
+        Word proxy = (Word) Proxy.newProxyInstance(
+                getClass().getClassLoader(),                    // 다이나믹 프록시가 정의되는 클래스 로더
+                new Class[]{Word.class},                        // 다이나믹 프록시가 구현해야 하는 인터페이스 (여러개 가능)
+                new UppercaseHandler(new WooWord())             // 핸들러 구현 오브젝트
+        );
+
+        // when & then
+        assertThat(proxy.sayHello()).isEqualTo("HELLO GWOOPRK!");
+        assertThat(proxy.sayThanks()).isEqualTo("THANK YOU GWOOPRK!");
     }
 }
