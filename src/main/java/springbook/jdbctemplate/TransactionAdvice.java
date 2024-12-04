8a0612fb41b55e2532809f9baee0e0016b291e4e
Woo <gwoprk98@gmail.com>
2024-12-04 21:39:58 +0900

    feat: Advisor를 이용해 transaction 적용 대상 설정

---
+++ src/main/java/springbook/jdbctemplate/TransactionAdvice.java
@@ -0,0 +1,36 @@
+package springbook.jdbctemplate;
+
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
+
+@Component
+public class TransactionAdvice implements MethodInterceptor {
+
+    private final PlatformTransactionManager transactionManager;
+
+    public TransactionAdvice(final DataSource dataSource) {
+        this.transactionManager = new JdbcTransactionManager(dataSource);
+    }
+
+    @Override
+    public Object invoke(final MethodInvocation invocation) throws Throwable {
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            Object result = invocation.proceed();
+            transactionManager.commit(status);
+            return result;
+        } catch (RuntimeException e) {
+            transactionManager.rollback(status);
+            throw e;
+        }
+    }
+}
