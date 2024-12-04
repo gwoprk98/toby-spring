561c96cc71cd9e542bf69713a7cbd673d46cc404
Woo <gwoprk98@gmail.com>
2024-12-04 20:54:23 +0900

    feat: Advisor를 이용해 부가 기능 및 적용 대상 설정

--- src/test/java/springbook/proxy/WordTest.java
+++ src/test/java/springbook/proxy/WordTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import java.lang.reflect.Proxy;
 
@@ -52,5 +53,26 @@ public class WordTest {
         assertThat(proxy.sayHello()).isEqualTo("HELLO GWOOPRK!");
 
         assertThat(proxy.sayThanks()).isEqualTo("THANK YOU GWOOPRK!");
+    }
+
+    @DisplayName(value = "Advisor를 통해 부가 기능 및 적용대상 설정")
+    @Test
+    void pointcut() {
+        // given
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.setTarget(new WooWord());
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("sayH*");
+
+        proxyFactoryBean.addAdvisor(new UppercaseAdvisor(pointcut, new UppercaseAdvice()));
+
+        Word proxy = (Word) proxyFactoryBean.getObject();
+
+        // when & then
+
+        assertThat(proxy.sayHello()).isEqualTo("HELLO GWOOPRK!");
+
+        assertThat(proxy.sayThanks()).isEqualTo("Thank you gwooprk!");
     }
 }
