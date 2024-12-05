aa247aa0d0264092b321017f9c837ee336dbae28
Woo <gwoprk98@gmail.com>
2024-12-05 13:52:59 +0900

    feat: Advisior 적용 확인을 위한 타깃 추가 및 테스트 추가

--- src/main/java/springbook/proxy/LogwooWord.java
+++ src/main/java/springbook/proxy/LogwooWord.java
@@ -1,4 +1,16 @@
 package springbook.proxy;
 
-public class LogwooWord {
+public class LogwooWord implements Word {
+
+    private static final String NAME = "wooword";
+
+    @Override
+    public String sayHello() {
+        return String.format("Hello %s!", NAME);
+    }
+
+    @Override
+    public String sayThanks() {
+        return String.format("Thank you %s!", NAME);
+    }
 }
--- src/test/java/springbook/proxy/ProxyFactoryBeanTest.java
+++ src/test/java/springbook/proxy/ProxyFactoryBeanTest.java
@@ -1,4 +1,42 @@
 package springbook.proxy;
 
-public class ProxyFactoryBeanTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProxyFactoryBeanTest {
+
+    @DisplayName(value = "ProxyFactoryBean을 이용한 proxy 체크")
+    @Test
+    void pointcut() {
+        //given
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setClassFilter(clazz -> clazz.getSimpleName().startsWith("Wooword"));
+        pointcut.setMappedName("sayH*");
+
+        validateAdvice(new WooWord(), pointcut, true);
+        validateAdvice(new LogwooWord(), pointcut, false);
+    }
+
+    private void validateAdvice(final Object target, final Pointcut pointcut, boolean isAdvice) {
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.setTarget(target);
+        proxyFactoryBean.addAdvisor(new UppercaseAdvisor(pointcut, new UppercaseAdvice()));
+
+        Word proxy = (Word) proxyFactoryBean.getObject();
+
+        // when & then
+        if (isAdvice) {
+            assertThat(proxy.sayHello()).startsWith("HELLO ");
+            assertThat(proxy.sayThanks()).startsWith("Thank you");
+            return;
+        }
+
+        assertThat(proxy.sayHello()).startsWith("Hello ");
+        assertThat(proxy.sayThanks()).startsWith("Thank you");
+    }
 }
