beb1861818018863411742c51e60a4a90d4f3c2e
Woo <gwoprk98@gmail.com>
2024-12-09 19:19:00 +0900

    refactor: sql  읽기와 저장 기능 분리

--- src/main/java/springbook/jdbctemplate/supporter/SqlFinder.java
+++ src/main/java/springbook/jdbctemplate/supporter/SqlFinder.java
@@ -2,36 +2,21 @@ package springbook.jdbctemplate.supporter;
 
 import org.springframework.stereotype.Component;
 
-import java.io.InputStream;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+
 
 @Component
 public class SqlFinder implements SqlService {
 
-    private final Map<String, String> properties = new HashMap<>();
+    private SqlRegistry sqlRegistry;
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        String resource = "sql-user.properties";
-        Properties properties = new Properties();
-
-        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(resource);
-        properties.load(inputStream);
-
-        Enumeration<String> enums = (Enumeration<String>) properties.propertyNames();
-
-        while (enums.hasMoreElements()) {
-            String key = enums.nextElement();
-            String value = properties.getProperty(key);
-            this.properties.put(key, value);
-        }
+       sqlRegistry = new SqlRegistryImpl();
+       new SqlReaderImpl().readSql(sqlRegistry);
     }
 
     @Override
     public String getSql(final String key) {
-        return this.properties.get(key);
+        return sqlRegistry.findSql(key);
     }
 }
